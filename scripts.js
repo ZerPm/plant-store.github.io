43b817e6f544cf6e4e15164b1b8d88f36b1e2c70
ZerPm <christian.manco3@gmail.com>
2025-03-06 14:58:58 -0500

    Initial Commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-let cartCount = 0;
+// Cargar el contador del carrito al iniciar la página
+let cartCount = localStorage.getItem("cartCount") ? parseInt(localStorage.getItem("cartCount")) : 0;
+updateCart();
+
 function updateCart() {
-    document.getElementById('cart-count').textContent = cartCount;
+    document.querySelectorAll('#cart-count').forEach(el => el.textContent = cartCount);
+}
+
+// Función para agregar productos al carrito
+function addToCart() {
+    cartCount++;
+    localStorage.setItem("cartCount", cartCount);
+    updateCart();
 }
